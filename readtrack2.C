3f13c08ddafffaa16e858c104dc41d102d99f4f3
hxwu <abwuge@163.com>
2024-12-05 22:49:54 +0800

    fix bug and draw points by track

--- readtrack2.C
+++ readtrack2.C
@@ -4,18 +4,17 @@
 #include <vector>
 #include <string>
 #include <limits>
-#include <unordered_set>
 
 #include "TMath.h"
 #include "TCanvas.h"
-#include "TPolyLine3D.h"
+#include "TGraph.h"
+#include "TGraph2D.h"
 #include "TObjArray.h"
 #include "TView.h"
 #include "TPaveText.h"
 #include "TLegend.h"
 #include "TMarker.h"
 #include "TString.h"
-
 
 using namespace std;
 
@@ -33,29 +32,10 @@ struct Point
     static const float epsilon;
 
     Point(float x_val, float y_val, float z_val) : x(x_val), y(y_val), z(z_val) {}
-
-    struct Hash
-    {
-        size_t operator()(const Point &p) const
-        {
-            size_t h1 = std::hash<int>()(static_cast<int>(p.x / epsilon));
-            size_t h2 = std::hash<int>()(static_cast<int>(p.y / epsilon));
-            size_t h3 = std::hash<int>()(static_cast<int>(p.z / epsilon));
-            return h1 ^ (h2 << 1) ^ (h3 << 2);
-        }
-    };
-
-    struct Equal
-    {
-        bool operator()(const Point &lhs, const Point &rhs) const
-        {
-            return std::abs(lhs.x - rhs.x) < epsilon && std::abs(lhs.y - rhs.y) < epsilon && std::abs(lhs.z - rhs.z) < epsilon;
-        }
-    };
 };
 const float Point::epsilon = 1e-2;
 
-Point getCorePosition(const string &fileName, size_t &particleID, float& energy, float &theta, float &phi);
+Point getCorePosition(const string &fileName, size_t &particleID, float &energy, float &theta, float &phi);
 
 int main(int argc, char *argv[])
 {
@@ -94,9 +74,9 @@ int main(int argc, char *argv[])
     }
 
     // Load all data into a 3D array: [file][record_index][variable]
-    vector<vector<vector<float>>> allData(fileNames.size());
+    vector<vector<vector<float>>> allData(particleTypes.size());
 
-    for (size_t i = 0; i < fileNames.size(); ++i)
+    for (size_t i = 0; i < particleTypes.size(); ++i)
     {
         const auto &fileName = fileNames[i];
         ifstream inputFile(fileName, ios::binary);
@@ -154,8 +134,8 @@ int main(int argc, char *argv[])
     tStart -= 4 * deltaT;                 // Start 4 time windows before the first particle
 
     // Find the starting index for each file
-    vector<size_t> allIdx(fileNames.size(), 0);
-    for (size_t fileIdx = 0; fileIdx < fileNames.size(); ++fileIdx)
+    vector<size_t> allIdx(particleTypes.size(), 0);
+    for (size_t fileIdx = 0; fileIdx < particleTypes.size(); ++fileIdx)
     {
         const auto &data = allData[fileIdx];
         auto it = lower_bound(data.begin(), data.end(), tStart, [](const vector<float> &vec, float value)
@@ -169,7 +149,7 @@ int main(int argc, char *argv[])
     Point corePosition = getCorePosition(baseFileName, particleID, energy, theta, phi);
     // set z range
     double zMin = corePosition.z, zMax = 0, xTop = 0, yTop = 0;
-    for (size_t fileIdx = 0; fileIdx < fileNames.size(); ++fileIdx)
+    for (size_t fileIdx = 0; fileIdx < particleTypes.size(); ++fileIdx)
         if (allIdx[fileIdx])
         {
             const auto particleInfo = allData[fileIdx][allIdx[fileIdx]];
@@ -182,19 +162,21 @@ int main(int argc, char *argv[])
 
     // set x, y range
     long double xAvg = 0, yAvg = 0;
-    unsigned long pcnt = 0;
-    for (const auto &data : allData)
-        for (const auto &particleInfo : data)
-        {
-            const auto &xEnd = 1e-2 * particleInfo[6], &yEnd = 1e-2 * particleInfo[7], &zEnd = 1e-2 * particleInfo[8];
-            if (zEnd - zMin < 0.1 * zMax)
+    { // Calculate the average x, y position of the particles
+        unsigned long pcnt = 0;
+        for (const auto &data : allData)
+            for (const auto &particleInfo : data)
             {
-                xAvg += xEnd;
-                yAvg += yEnd;
-                ++pcnt;
+                const auto &xEnd = 1e-2 * particleInfo[6], &yEnd = 1e-2 * particleInfo[7], &zEnd = 1e-2 * particleInfo[8];
+                if (zEnd - zMin < 0.1 * zMax)
+                {
+                    xAvg += xEnd;
+                    yAvg += yEnd;
+                    ++pcnt;
+                }
             }
-        }
-    xAvg /= pcnt, yAvg /= pcnt;
+        xAvg /= pcnt, yAvg /= pcnt;
+    }
 
     double xMin = xAvg - 2e3, xMax = xAvg + 2e3, yMin = yAvg - 2e3, yMax = yAvg + 2e3;
     if (xTop < xMin)
@@ -215,25 +197,21 @@ int main(int argc, char *argv[])
     TCanvas *canvas = new TCanvas("canvas", "Particle Tracks", 1414, 1000);
     canvas->Divide(2, 2);
 
-    TLegend *legend = new TLegend(0.05, 0.8, 0.6, 0.9);
-    legend->SetFillColor(0);
-    legend->SetBorderSize(0);
-    legend->SetNColumns(3);
-    legend->SetTextSize(0.05);
-    for (size_t i = 0; i < particleTypes.size(); ++i) {
+    TLegend *legend = new TLegend(0.15, 0.8, 0.72, 0.9);
+    { // Set the legend
+        legend->SetFillColor(0);
+        legend->SetBorderSize(0);
+        legend->SetNColumns(3);
+        legend->SetTextSize(0.05);
+    }
+    for (size_t i = 0; i < particleTypes.size(); ++i)
+    {
         TMarker *dummyMarker = new TMarker(0.5, 0.5, 20);
         dummyMarker->SetMarkerColor(particleTypes[i].color);
         legend->AddEntry(dummyMarker, particleTypes[i].name.c_str(), "p");
     }
 
-    vector<TView *> views(4);
-    Double_t viewAngles[4][3] = {
-        {-90, 90, 0}, // x-z plane
-        {0, 90, 0},   // y-z plane
-        {0, 0, -90},  // x-y plane
-        {45, 45, 0},  // 3D view
-    };
-    string titles[4] = {
+    string titleNames[4] = {
         "x-z Plane",
         "y-z Plane",
         "x-y Plane",
@@ -242,99 +220,93 @@ int main(int argc, char *argv[])
     for (size_t i = 0; i < 4; ++i)
     {
         canvas->cd(i + 1);
-        views[i] = TView::CreateView(1);
-        views[i]->SetRange(xMin, yMin, zMin, xMax, yMax, zMax);
-
-        Int_t irep = 0;
-        views[i]->SetView(viewAngles[i][0], viewAngles[i][1], viewAngles[i][2], irep);
-        views[i]->ShowAxis();
-
+        TView *view = TView::CreateView(1);
+        view->SetRange(xMin, yMin, zMin, xMax, yMax, zMax);
         TPaveText *title = new TPaveText(0.1, 0.9, 0.9, 1, "NDC");
         title->SetTextAlign(22);
-        title->SetTextSize(0.06); 
+        title->SetTextSize(0.06);
         title->SetFillColor(0);
         title->SetLineColor(0);
         title->SetShadowColor(0);
-        title->AddText(titles[i].c_str());
+        title->AddText(titleNames[i].c_str());
         title->Draw();
 
         legend->Draw();
     }
 
-    TPaveText *time = new TPaveText(0.7, 0.8, 0.9, 0.9, "NDC");
-    time->SetTextAlign(12);
-    time->SetTextSize(0.05);
-    time->SetTextFont(42);
-    time->SetFillColor(0);
-    time->SetLineColor(0);
-    time->SetShadowColor(0);
-
-    TObjArray *plot = new TObjArray();
-
-    // Record which line is used
-    unordered_set<Point, Point::Hash, Point::Equal> usedPoints;
-    // add the first point
-    for (size_t fileIdx = 0; fileIdx < fileNames.size(); ++fileIdx)
-    {
-        const auto &particleInfo = allData[fileIdx][allIdx[fileIdx]];
-        usedPoints.emplace(Point(1e-2 * particleInfo[2], 1e-2 * particleInfo[3], 1e-2 * particleInfo[4]));
+    TPaveText *time = new TPaveText(0.75, 0.8, 0.95, 0.9, "NDC");
+    { // Set the time display
+        time->SetTextAlign(12);
+        time->SetTextSize(0.05);
+        time->SetTextFont(42);
+        time->SetFillColor(0);
+        time->SetLineColor(0);
+        time->SetShadowColor(0);
     }
 
-    // Plot particle tracks
+    // Plot particles
+    TGraph2D *blankGraph = new TGraph2D();
+    blankGraph->SetPoint(0, 0, 0, 0);
+    blankGraph->SetMarkerSize(0);
     cout << "Generating PNG files ..." << endl;
     size_t frame = 0;
-    bool hasDrawed = false;
-    double currentT = tStart;
-    while (currentT <= tMax)
+    for (double currentT = tStart; currentT <= tMax; currentT += deltaT)
     {
-        plot->Clear();
+        vector<TGraph2D *> graphs2D(particleTypes.size(), nullptr);
+        vector<TGraph *> graphsXZ(particleTypes.size(), nullptr);
+        vector<TGraph *> graphsYZ(particleTypes.size(), nullptr);
+        vector<TGraph *> graphsXY(particleTypes.size(), nullptr);
+
+        for (size_t i = 0; i < 4; ++i)
+        {
+            canvas->cd(i + 1);
+            gPad->Clear();
+        }
+
         // Iterate over each file
-        for (size_t fileIdx = 0; fileIdx < fileNames.size(); ++fileIdx)
+        for (size_t fileIdx = 0; fileIdx < particleTypes.size(); ++fileIdx)
         {
             size_t cnt = 0;
+
             const auto &data = allData[fileIdx];
+
+            auto graphInit = [](TGraph *graph, int color)
+            {
+                graph->SetMarkerStyle(20);
+                graph->SetMarkerSize(0.5);
+                graph->SetMarkerColor(color);
+            };
+            auto graph2DInit = [](TGraph2D *graph, int color)
+            {
+                graph->SetMarkerStyle(20);
+                graph->SetMarkerSize(0.5);
+                graph->SetMarkerColor(color);
+            };
+
+            graphsXY[fileIdx] = new TGraph();
+            graphInit(graphsXY[fileIdx], particleTypes[fileIdx].color);
+            graphsXZ[fileIdx] = new TGraph();
+            graphInit(graphsXZ[fileIdx], particleTypes[fileIdx].color);
+            graphsYZ[fileIdx] = new TGraph();
+            graphInit(graphsYZ[fileIdx], particleTypes[fileIdx].color);
+            graphs2D[fileIdx] = new TGraph2D();
+            graph2DInit(graphs2D[fileIdx], particleTypes[fileIdx].color);
+
             for (auto &idx = allIdx[fileIdx]; idx < data.size(); ++idx)
             {
                 const auto &particleInfo = data[idx];
                 if (particleInfo[9] < currentT + deltaT)
                 {
-                    const auto &xStart = 1e-2 * particleInfo[2], &yStart = 1e-2 * particleInfo[3], &zStart = 1e-2 * particleInfo[4];
                     const auto &xEnd = 1e-2 * particleInfo[6], &yEnd = 1e-2 * particleInfo[7], &zEnd = 1e-2 * particleInfo[8];
-                    // Clean
-                    auto cleanLines = [&]()
-                    {
-                        if (!usedPoints.count(Point(xStart, yStart, zStart)))
-                            return true;
-                        if (zStart - zEnd < -1) // Filter out the particles that is moving upwards
-                            return true;
-                        if (xEnd < xMin || xEnd > xMax)
-                            return true;
-                        if (yEnd < yMin || yEnd > yMax)
-                            return true;
-                        if (++cnt > 1e3 && rand() % 100 < 75) // Randomly remove some lines
-                            return true;
-                        return false;
-                    };
 
-                    if (hasDrawed && cleanLines())
+                    if (xEnd < xMin || xEnd > xMax || yEnd < yMin || yEnd > yMax || zEnd < zMin || zEnd > zMax || cnt > 1e3)
                         continue;
 
-                    if (!hasDrawed)
-                        if (!cleanLines())
-                            hasDrawed = true;
-
-                    usedPoints.emplace(Point(xEnd, yEnd, zEnd));
-
-                    if (!hasDrawed && cleanLines())
-                        continue;
-
-                    // Add the particle track to the plot
-                    TPolyLine3D *line = new TPolyLine3D(2);
-                    line->SetPoint(0, xStart, yStart, zStart); // Start point
-                    line->SetPoint(1, xEnd, yEnd, zEnd);       // End point
-                    line->SetLineColor(particleTypes[fileIdx].color);
-                    line->SetLineWidth(1);
-                    plot->Add(line);
+                    // Add the particle to the graph
+                    graphsXZ[fileIdx]->SetPoint(cnt, xEnd, zEnd);
+                    graphsYZ[fileIdx]->SetPoint(cnt, yEnd, zEnd);
+                    graphsXY[fileIdx]->SetPoint(cnt, xEnd, yEnd);
+                    graphs2D[fileIdx]->SetPoint(cnt++, xEnd, yEnd, zEnd);
                 }
                 else
                     break;
@@ -342,41 +314,101 @@ int main(int argc, char *argv[])
         }
 
         // Draw the accumulated data
-
-        size_t entries = plot->GetEntries();
-        printf("Frame %d (time: %.0fns): add %d line%s;\n", ++frame, currentT * 1e9, entries, entries < 2 ? "" : "s");
-
         time->Clear();
         time->AddText(TString::Format("Time: %.2f #mus", currentT * 1e6));
-        for (size_t i = 0; i < 4; ++i)
+
+        auto drawGraphs = [&](const vector<TGraph *> &graphs, size_t i)
         {
-            canvas->cd(i + 1);
-            plot->Draw("same");
+            canvas->cd(i);
+
+            if (i == 1) // x-z plane
+                gPad->DrawFrame(xMin, zMin, xMax, zMax, ";X;Z");
+            else if (i == 2) // y-z plane
+                gPad->DrawFrame(yMin, zMin, yMax, zMax, ";Y;Z");
+            else if (i == 3) // x-y plane
+                gPad->DrawFrame(xMin, yMin, xMax, yMax, ";X;Y");
+
+            TPaveText *title = new TPaveText(0.1, 0.9, 0.9, 1, "NDC");
+            { // Set the title
+                title->SetTextAlign(22);
+                title->SetTextSize(0.06);
+                title->SetFillColor(0);
+                title->SetLineColor(0);
+                title->SetShadowColor(0);
+                title->AddText(titleNames[i - 1].c_str());
+            }
+            title->Draw();
+
+            legend->Draw();
+
+            for (size_t fileIdx = 0; fileIdx < particleTypes.size(); ++fileIdx)
+                if (graphs[fileIdx]->GetN())
+                    graphs[fileIdx]->Draw("P same");
+
             time->Draw();
-        }
+        };
+        auto drawGraphs2D = [&](const vector<TGraph2D *> &graphs, size_t i)
+        {
+            canvas->cd(i);
+
+            TView *view = TView::CreateView(1);
+            view->SetRange(xMin, yMin, zMin, xMax, yMax, zMax);
+            view->ShowAxis();
+
+            TPaveText *title = new TPaveText(0.1, 0.9, 0.9, 1, "NDC");
+            { // Set the title
+                title->SetTextAlign(22);
+                title->SetTextSize(0.06);
+                title->SetFillColor(0);
+                title->SetLineColor(0);
+                title->SetShadowColor(0);
+                title->AddText(titleNames[i - 1].c_str());
+            }
+            title->Draw();
+
+            legend->Draw();
+
+            blankGraph->Draw("P same");
+            for (size_t fileIdx = 0; fileIdx < particleTypes.size(); ++fileIdx)
+                if (graphs[fileIdx]->GetN())
+                    graphs[fileIdx]->Draw("P same");
+            time->Draw();
+        };
+
+        drawGraphs(graphsXZ, 1);
+        drawGraphs(graphsYZ, 2);
+        drawGraphs(graphsXY, 3);
+        drawGraphs2D(graphs2D, 4);
+
+        size_t pointNum = 0;
+        for (size_t fileIdx = 0; fileIdx < graphs2D.size(); ++fileIdx)
+            pointNum += graphs2D[fileIdx]->GetN();
+
+        printf("Frame %2d (time: %6.0fns): add %3d point%s;\n", ++frame, currentT * 1e9, pointNum, pointNum < 2 ? "" : "s");
+
         canvas->Update();
         canvas->Print(TString::Format("%s%.10f.png", pngPath.c_str(), currentT).Data(), "png");
 
-        currentT += deltaT; // Move to the next time window
+        for (auto graph : graphs2D)
+            delete graph;
     }
-    canvas->Print(TString::Format("%sPID%d_E%.1f_T%.1f_P%.1fl_end.png", PNGPath.c_str(), particleID, energy, theta * 57.3, phi * 57.3).Data(), "png");
     cout << "PNG generated successfully!" << endl;
     cout << "Generating GIF file ..." << endl;
-    string gifName = TString::Format("%sPID%d_E%.1f_T%.1f_P%.1fl.gif", PNGPath.c_str(), particleID, energy, theta * 57.3, phi * 57.3).Data();
+    string gifName = TString::Format("%sPID%d_E%.1f_T%.1f_P%.1fp.gif", PNGPath.c_str(), particleID, energy, theta * 57.3, phi * 57.3).Data();
     system(("convert " + pngPath + "*.png " + gifName).c_str());
     cout << "GIF generated successfully: " << gifName << endl;
 
     // Cleanup
     cout << "Cleaning ..." << endl;
     system(("rm -r " + pngPath).c_str());
-    delete plot;
+
     delete canvas;
     cout << "Cleaned" << endl;
 
     return 0;
 }
 
-Point getCorePosition(const string &fileName, size_t &particleID, float& energy, float &theta, float &phi)
+Point getCorePosition(const string &fileName, size_t &particleID, float &energy, float &theta, float &phi)
 {
     Point corePosition(0, 0, 0);
 
